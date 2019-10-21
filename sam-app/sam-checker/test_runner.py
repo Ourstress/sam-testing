1a831c915b010ce0031bc5645af41f5862f58220
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-10-21 14:47:43 +0800

    Copied package contents to tmp

--- sam-app/sam-checker/test_runner.py
+++ sam-app/sam-checker/test_runner.py
@@ -3,30 +3,34 @@ import json
 
 
 def store_content(shownCode, editedCode):
-    # app.py            - shownCode
+    # tests.py          - shownCode
     # template.yaml     - editedCode
     os.popen("mkdir -p /tmp/myProject").read()
     with open('/tmp/myProject/template.yaml', 'w') as outFile:
         outFile.write(editedCode)
-    with open('/tmp/myProject/app.py', 'w') as outFile:
+    with open('/tmp/myProject/tests.py', 'w') as outFile:
         outFile.write(shownCode)
 
 
-def execute():
+def setup_env():
+    os.popen("cp -r -u /var/task/sam-checker/* /tmp/myProject").read()
 
+
+def execute():
+    setup_env()
     # Get text feedback of unit test results
-    runUnitTest = 'cd /tmp/myProject; python3 -m unittest -v app.py 2>&1'
+    runUnitTest = 'cd /tmp/myProject; python3 -m unittest -v tests.py 2>&1'
     unitTestResults = os.popen(runUnitTest).read()
 
     # Get html feedback of unit test results
-    command = "cd /tmp/myProject; python3 -m unittest -v app.py 2>&1 | \
-                /var/task/ansi2html.sh > /tmp/myProject/unitTestResults.html"
+    command = "cd /tmp/myProject; python3 -m unittest -v tests.py 2>&1 | \
+                /tmp/myProject/ansi2html.sh > /tmp/myProject/unitTestResults.html"
     os.popen(command).read()
     htmlOutput = "<h2>Python unittest results</h2><br/>"
     htmlOutput += os.popen("cat /tmp/myProject/unitTestResults.html").read()
 
     # Get JSON feedback of unit test results
-    command = "python3 -m pytest --json-report -v /tmp/myProject/app.py \
+    command = "python3 -m pytest --json-report -v /tmp/myProject/tests.py \
             --json-report-summary --json-report-file /tmp/myProject/unitTestResults.json"
     os.popen(command).read()
     jsonOutput = json.loads(

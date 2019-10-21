0e4cba2df70a19a7ea15fb4400fba131139e6828
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-10-21 14:18:50 +0800

    Added python unitests

--- sam-app/sam-checker/test_runner.py
+++ sam-app/sam-checker/test_runner.py
@@ -1,6 +1,5 @@
 import os
 import json
-import yaml
 
 
 def store_content(shownCode, editedCode):
@@ -14,17 +13,42 @@ def store_content(shownCode, editedCode):
 
 
 def execute():
-    result = "Empty"
-    with open('/tmp/myProject/template.yaml', 'r') as inFile:
-        data_loaded = yaml.safe_load(inFile)
-        result = data_loaded
+
+    # Get text feedback of unit test results
+    runUnitTest = 'cd /tmp/myProject; python3 -m unittest -v app.py 2>&1'
+    unitTestResults = os.popen(runUnitTest).read()
+
+    # Get html feedback of unit test results
+    command = "cd /tmp/myProject; python3 -m unittest -v app.py 2>&1 | \
+                /var/task/ansi2html.sh > /tmp/myProject/unitTestResults.html"
+    os.popen(command).read()
+    htmlOutput = "<h2>Python unittest results</h2><br/>"
+    htmlOutput += os.popen("cat /tmp/myProject/unitTestResults.html").read()
+
+    # Get JSON feedback of unit test results
+    command = "python3 -m pytest --json-report -v /tmp/myProject/app.py \
+            --json-report-summary --json-report-file /tmp/myProject/unitTestResults.json"
+    os.popen(command).read()
+    jsonOutput = json.loads(
+        os.popen("cat /tmp/myProject/unitTestResults.json").read())
+
+    # Get isComplete
+    if ("summary" in jsonOutput):
+        if("failed" in jsonOutput["summary"]):
+            isComplete = False
+        elif(jsonOutput["summary"].get("total", 0) == jsonOutput["summary"].get("passed", 1)):
+            isComplete = True
+        else:
+            isComplete = False
+    else:
+        isComplete = False
 
     # Form the response
     results = {
-        "isComplete": True,
-        "htmlFeedback": result,
-        "jsonFeedback": result,
-        "textFeedback": result
+        "isComplete": isComplete,
+        "htmlFeedback": htmlOutput,
+        "jsonFeedback": jsonOutput,
+        "textFeedback": unitTestResults
     }
 
     return json.dumps(results)
